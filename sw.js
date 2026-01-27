8591db75d4117854d6c45c2e11baf4204042144a
Kaname Sadoyama <sady225@gmail.com>
2026-01-27 19:48:22 +0900

    UP

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'teaunder-universe-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@300;400;700&display=swap'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// リクエスト処理
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
+
+// 古いキャッシュの削除
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
