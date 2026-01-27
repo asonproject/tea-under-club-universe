67fb1779bbad3b7c65e8015566bdb865e2c57e27
Kaname Sadoyama <sady225@gmail.com>
2026-01-27 20:20:53 +0900

    UP
    アイコンを保存できるように

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-const CACHE_NAME = 'teaunder-universe-v1';
+const CACHE_NAME = 'teaunder-universe-v2';
 const ASSETS_TO_CACHE = [
   './index.html',
   './manifest.json',
+  './icon-192.jpeg',
+  './icon-512.jpeg',
   'https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@300;400;700&display=swap'
 ];
 
